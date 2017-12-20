538e4b99197e4a1f06d57b75b49a041b7b5de66a
Seth Greylyn <seth.greylyn@gmail.com>
2017-12-19 22:40:57 -0800

    Removed the call to dwebp in the worker, since the worker cannot access the DOM, which dwebp requires.

--- source/dwebp-worker.js
+++ source/dwebp-worker.js
@@ -3,6 +3,9 @@ self.importScripts("../dist/dwebp.js");
 
 onmessage = function(e) {
 	var webpdata = e.data[0];
-	var result = dwebp.webpToSdl(webpdata, webpdata.length);
+	// dwebp currently needs access to the canvas, which is currently impossible in a worker thread.
+	// This method is only useful as a picture into the future for now.
+	//var result = dwebp.webpToSdl(webpdata, webpdata.length);
+	var result = webpdata;
 	postMessage(result);
 }
